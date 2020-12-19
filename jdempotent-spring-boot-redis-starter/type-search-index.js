f5955632fedd0982dcb4e465dfbdfc483ee8a103
memojja <memojja1907@gmail.com>
2020-12-19 16:28:32 +0300

    add jdempotent redis starter javadoc

---
+++ jdempotent-spring-boot-redis-starter/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.trendyol.jdempotent.redis","l":"ApplicationConfig"},{"p":"com.trendyol.jdempotent.redis","l":"RedisConfigProperties"},{"p":"com.trendyol.jdempotent.redis","l":"RedisIdempotentRepository"},{"p":"com.trendyol.jdempotent.redis","l":"RedisSentinelConfiguration"}]
